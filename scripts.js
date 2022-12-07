07066a3eab0949d0f2c77df6833a182e611744a9
Gregório Francisco <gregoriofrancisco2014@gmail.com>
2022-12-07 06:27:53 +0100

    Chance currency (BRL to AOA)

--- scripts.js
+++ scripts.js
@@ -109,9 +109,9 @@ const Utils = {
     
     value = Number(value) / 100;
     
-    value = value.toLocaleString('pt-BR', {
+    value = value.toLocaleString('pt-PT', {
       style: 'currency',
-      currency: 'BRL'
+      currency: 'AOA'
     });
 
     return (signal + value)
@@ -188,4 +188,5 @@ const App = {
     App.init()
   }
 }
+
 App.init();
